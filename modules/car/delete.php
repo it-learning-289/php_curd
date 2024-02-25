efb016ab30b6e4e45abb6e5c6921c87e469b3960
tungtv289 <tungtv289@gmail.com>
2024-02-25 14:01:56 +0700

    refactor api for module

--- modules/car/delete.php
+++ modules/car/delete.php
@@ -3,7 +3,7 @@ $pathInfoArr1 = explode("/", $_SERVER["PATH_INFO"]);
 if ($pathInfoArr1[2] = 'cars') {
     $module = $pathInfoArr1[3];
     // var_dump($pathInfoArr1[3]);
-    var_dump($module);
+    // var_dump($module);
 
 
     $stmt = $pdo->prepare("DELETE FROM cars WHERE id = :id");
@@ -14,8 +14,16 @@ if ($pathInfoArr1[2] = 'cars') {
     // Execute the statement
     try {
         $stmt->execute();
+        // Check if the delete operation affected any rows
+        if ($stmt->rowCount() == 0) {
+            // Handle the case where 0 rows were affected
+            $message = "No matching records found for deletion";
+        } else {
+            // Handle the successful delete operation
+            $message = "Records deleted successfully";
+        }
         http_response_code(200); // OK
-        echo json_encode(array("message" => "Record deleted successfully."));
+        echo json_encode(array("message" => $message));
     } catch (PDOException $e) {
         http_response_code(500); // Internal Server Error
         echo json_encode(array("message" => "Failed to delete record: " . $e->getMessage()));
