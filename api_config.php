f8f8cbfffd8b76059ee3511295b1941714e158a6
itsTranT1706 <khiemtrangia60@gmail.com>
2024-03-04 00:43:10 +0700

    get_role_users sql

--- api_config.php
+++ api_config.php
@@ -19,12 +19,13 @@ $apiMap = [
     "/roles/post" =>  ["path" => "./modules/role/postRole.php@a"],
     "/roles/delete" =>  ["path" => "./modules/role/deleteRole.php@a"],
     "/roles/get_user_roles/get" =>  ["path" => "./modules/role/get_user_roles.php@a"],
+    "/roles/get_role_users/get" =>  ["path" => "./modules/role/get_role_users.php@a"],
     "/roles/get" =>  ["path" => "./modules/role/getRole.php@a"]
 ];
 $method = strtolower($_SERVER['REQUEST_METHOD']);
 // dd($method);
 $request = explode("/", $_SERVER["PATH_INFO"]);
-// dd($request[2]);
+// dd($request[3]);
 // dd(explode("/", "/shoes\/(\d.*)/delete")[1]);
 foreach ($apiMap as $key => $value) {
     //   d( explode("/", $key)[1] ." " .explode("/", $key)[2]);
@@ -32,7 +33,7 @@ foreach ($apiMap as $key => $value) {
     //  dd(explode("/", "/cars/{id}/delete"));
 
     if (($request[2] === explode("/", $key)[1]) && ($method === explode("/", $key)[2])) {
-     
+
         // dd(explode("/", "get_user_roles"));
         // dd(explode("@", $value["path"])); 
         $temp = explode("@", $value["path"])[0];
@@ -42,18 +43,19 @@ foreach ($apiMap as $key => $value) {
         } else {
             checkAuthForApi();
             $decoded_username = getUsernamePassFromToken()[0];
-            
+
             if (!limitPermition($decoded_username, $key)) {
                 echo json_encode(array("message" => "not allow"));
                 exit;
             }
             require_once $temp;
-            // exit;
+            exit;
         }
-    } else  if (($request[2] === explode("/", $key)[1]) && (($method === explode("/", $key)[3]) || ($method === explode("/", $key)[4]))) {
+    } else  if (($request[2] === explode("/", $key)[1]) && ($request[3] === explode("/", $key)[2]) && (($method === explode("/", $key)[3]) || ($method === explode("/", $key)[4]))) {
         // dd(explode("@", $value["path"])); 
         // dd("explode("/", $key)");
         // d($request);
+        // dd(explode("/", $key)[2]);
         $temp = explode("@", $value["path"])[0];
         checkAuthForApi();
         $decoded_username = getUsernamePassFromToken()[0];
